c493214de1107cb6ee69f5340c2d56bf18c99099
AlexandraTsai <b02105034@gmail.com>
2020-03-22 21:43:38 +0800

    Add Alamofire and Kanna

---
+++ Maps/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Maps
+//
+//  Created by 蔡佳宣 on 2020/3/22.
+//  Copyright © 2020 蔡佳宣. All rights reserved.
+//
+
+import SwiftUI
+import Alamofire
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+        
+        //*[@id="mapGraphic"]/g
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+func loadData() {
+    
+}
